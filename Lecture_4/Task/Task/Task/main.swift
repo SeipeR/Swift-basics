25045d645d292338d98e373d6fb0957e20698617
SeipeR <se1per@yandex.ru>
2021-03-02 16:41:04 +0300

    Выполнено задание 6
    Экземпляры объектов выведены в консоль;
    Проведён рефакторинг кода

--- Lecture_4/Task/Task/Task/main.swift
+++ Lecture_4/Task/Task/Task/main.swift
@@ -92,14 +92,19 @@ class TruckCar: Car {
         case rock = "Камни"
     }
     
+    enum IsSemitrailerPulling: String {
+        case installed = "Установлен"
+        case notInstalled = "Не установлен"
+    }
+    
     let truckType: TruckCarTypes
     let cargoType: CargoTypes
-    var isSemitrailerPulling: Bool
+    var semitrailer: IsSemitrailerPulling
     
-    init(truckType: TruckCarTypes, cargoType: CargoTypes, semitrailerPulling: Bool, model: CarModels, year: UInt16, capacity: UInt32, color: CarColors, engineState: EngineStates, windowState: WindowStates, occupiedCapacity: UInt32) {
+    init(truckType: TruckCarTypes, cargoType: CargoTypes, semitrailer: IsSemitrailerPulling, model: CarModels, year: UInt16, capacity: UInt32, color: CarColors, engineState: EngineStates, windowState: WindowStates, occupiedCapacity: UInt32) {
         self.truckType = truckType
         self.cargoType = cargoType
-        isSemitrailerPulling = semitrailerPulling
+        self.semitrailer = semitrailer
         
         super.init(model: model, year: year, capacity: capacity, color: color, engineState: engineState, windowState: windowState, occupiedCapacity: occupiedCapacity)
     }
@@ -112,21 +117,21 @@ class TruckCar: Car {
                     return occupiedTrunkCapacity
                 }
                 occupiedTrunkCapacity += capacity
-                print("В автомобиль типа \(truckType) загружено \(capacity)")
+                print("В автомобиль типа \(truckType.rawValue) загружено \(capacity)")
             case .unload:
                 guard capacity <= occupiedTrunkCapacity else {
                     print("Максимально возможное значение объёма для выгрузки: \(occupiedTrunkCapacity). \nВозвращено значение по умолчанию: \(occupiedTrunkCapacity)")
                     return occupiedTrunkCapacity
                 }
                 occupiedTrunkCapacity -= capacity
-                print("Из автомобиля типа \(truckType) выгружено \(capacity)")
+                print("Из автомобиля типа \(truckType.rawValue) выгружено \(capacity)")
         }
-        print("Текущий занятый объём автомобиля типа \(truckType): \(occupiedTrunkCapacity)")
+        print("Текущий занятый объём автомобиля типа \(truckType.rawValue): \(occupiedTrunkCapacity)")
         return occupiedTrunkCapacity
     }
     
     override func createCarDescription() {
-        print("Описание автомобиля:\nТип грузового автомобиля: \(truckType.rawValue)\nМарка автомобиля: \(model.rawValue)\nГод выпуска: \(year)\nЦвет автомобиля: \(color.rawValue)\nПеревозимый груз: \(cargoType)\nИмеется полуприцеп: \(isSemitrailerPulling)\nВмещаемый объём груза: \(trunkCapacity)\nЗанятый объём: \(occupiedTrunkCapacity)\n\(isEngineStart.rawValue)\n\(isWindowOpen.rawValue)\n\n")
+        print("Описание автомобиля:\nТип грузового автомобиля: \(truckType.rawValue)\nМарка автомобиля: \(model.rawValue)\nГод выпуска: \(year)\nЦвет автомобиля: \(color.rawValue)\nПеревозимый груз: \(cargoType.rawValue)\nИмеется полуприцеп: \(semitrailer.rawValue)\nВмещаемый объём груза: \(trunkCapacity)\nЗанятый объём: \(occupiedTrunkCapacity)\n\(isEngineStart.rawValue)\n\(isWindowOpen.rawValue)\n\n")
     }
 }
 
@@ -143,16 +148,21 @@ class SportCar: Car {
         case centre = "Центральное расположение двигателя"
     }
     
+    enum IsSpoilerInstalled: String {
+        case installed = "Установлен"
+        case notInstalled = "Не установлен"
+    }
+    
     let sportCarType: SportCarType
     let engineLocation: EngineLocation
     let maximumSpeed: UInt16
-    var isSpoilerInstalled: Bool
+    var spoiler: IsSpoilerInstalled
     
-    init(type: SportCarType, engineLocation: EngineLocation, maximumSpeed: UInt16, spoiler: Bool,model: CarModels, year: UInt16, capacity: UInt32, color: CarColors, engineState: EngineStates, windowState: WindowStates, occupiedCapacity: UInt32) {
+    init(type: SportCarType, engineLocation: EngineLocation, maximumSpeed: UInt16, spoiler: IsSpoilerInstalled,model: CarModels, year: UInt16, capacity: UInt32, color: CarColors, engineState: EngineStates, windowState: WindowStates, occupiedCapacity: UInt32) {
         sportCarType = type
         self.engineLocation = engineLocation
         self.maximumSpeed = maximumSpeed
-        isSpoilerInstalled = spoiler
+        self.spoiler = spoiler
         
         super.init(model: model, year: year, capacity: capacity, color: color, engineState: engineState, windowState: windowState, occupiedCapacity: occupiedCapacity)
     }
@@ -165,39 +175,49 @@ class SportCar: Car {
                     return occupiedTrunkCapacity
                 }
                 occupiedTrunkCapacity += capacity
-                print("В автомобиль типа \(sportCarType) загружено \(capacity)")
+                print("В автомобиль типа \(sportCarType.rawValue) загружено \(capacity)")
             case .unload:
                 guard capacity <= occupiedTrunkCapacity else {
                     print("Максимально возможное значение объёма для выгрузки: \(occupiedTrunkCapacity). \nВозвращено значение по умолчанию: \(occupiedTrunkCapacity)")
                     return occupiedTrunkCapacity
                 }
                 occupiedTrunkCapacity -= capacity
-                print("Из автомобиля типа \(sportCarType) выгружено \(capacity)")
+                print("Из автомобиля типа \(sportCarType.rawValue) выгружено \(capacity)")
         }
-        print("Текущий занятый объём автомобиля типа \(sportCarType): \(occupiedTrunkCapacity)")
+        print("Текущий занятый объём автомобиля типа \(sportCarType.rawValue): \(occupiedTrunkCapacity)")
         return occupiedTrunkCapacity
     }
     
     override func createCarDescription() {
-        print("Описание автомобиля:\nТип спортивного автомобиля: \(sportCarType.rawValue)\nМарка автомобиля: \(model.rawValue)\nГод выпуска: \(year)\nЦвет автомобиля: \(color.rawValue)\nРасположение двигателя: \(engineLocation)\nМаксимальная скорость: \(maximumSpeed)\nУстановлен спойлер: \(isSpoilerInstalled)\nВмещаемый объём груза: \(trunkCapacity)\nЗанятый объём: \(occupiedTrunkCapacity)\n\(isEngineStart.rawValue)\n\(isWindowOpen.rawValue)\n\n")
+        print("Описание автомобиля:\nТип спортивного автомобиля: \(sportCarType.rawValue)\nМарка автомобиля: \(model.rawValue)\nГод выпуска: \(year)\nЦвет автомобиля: \(color.rawValue)\nРасположение двигателя: \(engineLocation.rawValue)\nМаксимальная скорость: \(maximumSpeed)\nУстановлен спойлер: \(spoiler.rawValue)\nВмещаемый объём груза: \(trunkCapacity)\nЗанятый объём: \(occupiedTrunkCapacity)\n\(isEngineStart.rawValue)\n\(isWindowOpen.rawValue)\n\n")
     }
 }
 
-var truck1 = TruckCar(truckType: .bulldozer, cargoType: .rock, semitrailerPulling: true, model: .renault, year: 2006, capacity: 30000, color: .blue, engineState: .start, windowState: .close, occupiedCapacity: 8000)
-var truck2 = TruckCar(truckType: .fuel, cargoType: .fuel, semitrailerPulling: true, model: .ford, year: 1999, capacity: 20000, color: .black, engineState: .stop, windowState: .open, occupiedCapacity: 0)
-var truck3 = TruckCar(truckType: .panel, cargoType: .flower, semitrailerPulling: false, model: .renault, year: 2015, capacity: 5000, color: .red, engineState: .start, windowState: .close, occupiedCapacity: 1500)
+var truck1 = TruckCar(truckType: .bulldozer, cargoType: .rock, semitrailer: .installed, model: .renault, year: 2006, capacity: 30000, color: .blue, engineState: .start, windowState: .close, occupiedCapacity: 8000)
+var truck2 = TruckCar(truckType: .fuel, cargoType: .fuel, semitrailer: .installed, model: .ford, year: 1999, capacity: 20000, color: .black, engineState: .stop, windowState: .open, occupiedCapacity: 0)
+var truck3 = TruckCar(truckType: .panel, cargoType: .flower, semitrailer: .notInstalled, model: .renault, year: 2015, capacity: 5000, color: .red, engineState: .start, windowState: .close, occupiedCapacity: 1500)
 
 truck3.createCarDescription()
 truck3.color = .green
 truck1.changeOccupiedCapacity(action: .unload, capacity: 40000)
 truck2.changeOccupiedCapacity(action: .load, capacity: 14030)
-truck3.isSemitrailerPulling = false
+truck3.semitrailer = .installed
 
-var sportCar1 = SportCar(type: .sportCar, engineLocation: .centre, maximumSpeed: 250, spoiler: false, model: .audi, year: 2018, capacity: 300, color: .yellow, engineState: .stop, windowState: .close, occupiedCapacity: 50)
-var sportCar2 = SportCar(type: .superCar, engineLocation: .rear, maximumSpeed: 300, spoiler: true, model: .mazda, year: 2020, capacity: 200, color: .black, engineState: .start, windowState: .open, occupiedCapacity: 0)
-var sportCar3 = SportCar(type: .hyperCar, engineLocation: .centre, maximumSpeed: 370, spoiler: true, model: .bmw, year: 2019, capacity: 50, color: .yellow, engineState: .stop, windowState: .open, occupiedCapacity: 10)
+var sportCar1 = SportCar(type: .sportCar, engineLocation: .centre, maximumSpeed: 250, spoiler: .notInstalled, model: .audi, year: 2018, capacity: 300, color: .yellow, engineState: .stop, windowState: .close, occupiedCapacity: 50)
+var sportCar2 = SportCar(type: .superCar, engineLocation: .rear, maximumSpeed: 300, spoiler: .installed, model: .mazda, year: 2020, capacity: 200, color: .black, engineState: .start, windowState: .open, occupiedCapacity: 0)
+var sportCar3 = SportCar(type: .hyperCar, engineLocation: .centre, maximumSpeed: 370, spoiler: .installed, model: .bmw, year: 2019, capacity: 50, color: .yellow, engineState: .stop, windowState: .open, occupiedCapacity: 10)
 
 sportCar3.isEngineStart = .start
 sportCar2.isWindowOpen = .close
-sportCar1.isSpoilerInstalled = true
+sportCar1.spoiler = .installed
 sportCar3.changeOccupiedCapacity(action: .unload, capacity: 10)
+
+var carsList = [truck1, truck2, truck3, sportCar1, sportCar2, sportCar3]
+
+func createDescription(array: Array<Car>) {
+    for element in array {
+        element.createCarDescription()
+    }
+}
+
+createDescription(array: carsList)
