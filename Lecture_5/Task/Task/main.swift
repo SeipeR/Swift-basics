340d9fea5c167b054e390eb05ae4b121ac574a8a
SeipeR <se1per@yandex.ru>
2021-03-08 16:47:22 +0300

    Выполнено задание 2
    Создано расширение для протокола

--- Lecture_5/Task/Task/main.swift
+++ Lecture_5/Task/Task/main.swift
@@ -42,7 +42,45 @@ protocol Car {
     var isWindowOpen: WindowStates { get set }
     var occupiedTrunkCapacity: UInt32 { get set }
 
-//    func changeOccupiedCapacity(action: TrunkActions, capacity: UInt32) -> UInt32
-
     func createCarDescription()
 }
+
+extension Car {
+    mutating func changeOccupiedCapacity(action: TrunkActions, capacity: UInt32) -> UInt32 {
+        switch action {
+            case .load:
+                guard (occupiedTrunkCapacity + capacity) <= trunkCapacity else {
+                    print("Максимально возможное значение объёма для погрузки: \(trunkCapacity - occupiedTrunkCapacity). \nВозвращено значение по умолчанию: \(occupiedTrunkCapacity)")
+                    return occupiedTrunkCapacity
+                }
+                occupiedTrunkCapacity += capacity
+                print("В автомобиль загружено \(capacity)")
+            case .unload:
+                guard capacity <= occupiedTrunkCapacity else {
+                    print("Максимально возможное значение объёма для выгрузки: \(occupiedTrunkCapacity). \nВозвращено значение по умолчанию: \(occupiedTrunkCapacity)")
+                    return occupiedTrunkCapacity
+                }
+                occupiedTrunkCapacity -= capacity
+                print("Из автомобиля выгружено \(capacity)")
+        }
+        print("Текущий занятый объём автомобиля: \(occupiedTrunkCapacity)")
+        return occupiedTrunkCapacity
+    }
+    
+    mutating func changeCarColor(newColor: CarColors) {
+        print("Цвет автомобиля изменён с \(color) на \(newColor.rawValue)")
+        color = newColor
+    }
+    
+    mutating func changeEngiteState (newEngineState: EngineStates) {
+        print(newEngineState.rawValue)
+        isEngineStart = newEngineState
+    }
+    
+    mutating func changeWindowState (newWindowState: WindowStates) {
+        print(newWindowState.rawValue)
+        isWindowOpen = newWindowState
+    }
+}
+
+
